0db6259f7265d7cdc492d9c557ab71f68985d746
ladishala <lavdishala@hotmail.com>
2014-04-20 01:28:57 +0200

    Validations added

--- src/com/example/adresat/KrijoUser.java
+++ src/com/example/adresat/KrijoUser.java
@@ -1,14 +1,15 @@
 package com.example.adresat;
 
-import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-
-
-
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -37,11 +38,13 @@ public class KrijoUser extends FragmentActivity implements OnFocusChangeListener
 	TextView Mesazhi;
 	Spinner spLista;
 	ProgressBar objProgress;
-	boolean errorNukEgziston;
-	boolean errorIregjistruar;
-	boolean errorPassword;
-	boolean errorPasswordMatch;
-	boolean errorEmail;
+	boolean valid=false;
+	boolean errorNukEgziston=false;
+	boolean errorIregjistruar=false;
+	String Gabimi="";
+	AlertDialog.Builder Alert;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)  {
 		super.onCreate(savedInstanceState);
@@ -59,7 +62,7 @@ public class KrijoUser extends FragmentActivity implements OnFocusChangeListener
 		Mesazhi = (TextView)findViewById(R.id.txtMesazhi);
 		spLista = (Spinner)findViewById(R.id.spLloji);
 		objProgress = (ProgressBar)findViewById(R.id.progressBar1);
-		editPassword2.setOnFocusChangeListener(this);
+		editUsername.setOnFocusChangeListener(this);
 		
 		
 		
@@ -76,29 +79,105 @@ public class KrijoUser extends FragmentActivity implements OnFocusChangeListener
 	
 	public void regjistrohuClick(View v)
 	{
-		String Username=editUsername.getText().toString();
-		String Hash1 = hash(editPassword.getText().toString());
-		String Salt=gjeneroSalt();
-		Hash1 = Hash1+Salt;
-		String Hash = hash(Hash1);
+		Gabimi="";
+		valid=false;
+		(new validouserTask()).execute(editUsername.getText().toString(),"Individ");
+		
+	}
+	private boolean validuesi()
+	{
+		String Password = editPassword.getText().toString();
 		String Email = editEmail.getText().toString();
-		Mesazhi.setVisibility(0);
-		Mesazhi.setText(Hash);
-		(new shtouserTask()).execute(Username,Hash,Salt,Email);
+		String UserName = editUsername.getText().toString();
+		boolean dalja = true;
+		
+		if(Password.length()==0)
+		{
+			Gabimi ="Ju lutem zgjedhni nje password";
+			editPassword2.setText("");
+			dalja=false;			
+		}
+		else if(!(editPassword.getText().toString().equals(editPassword2.getText().toString())))
+		{
+			Gabimi ="Passwordet nuk perputhen";
+			editPassword.setText("");
+			editPassword2.setText("");
+			dalja=false;
+		}
+		
+		else if(!regex("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[0-9A-Za-z]{8,}$",Password))
+		{
+			Gabimi ="Keni zgjedhur password jo te sigurt.";
+			editPassword.setText("");
+			editPassword2.setText("");
+			dalja=false;
+			
+		}
+		
+		if(!android.util.Patterns.EMAIL_ADDRESS.matcher(Email).matches() || Email.length()==0)
+		{
+			Gabimi += "\nEmail eshte jovalid.";
+			editEmail.setText("");
+			dalja = false;	
+		}
+		
+		int l = UserName.length();
+		if(errorNukEgziston || (l!=10 && l!=8 && l!= 9) )
+		{
+			Gabimi +="\nKeni dhene Username jovalid.";
+			editUsername.setText("");
+			dalja=false;
+		}
+		
+		else if(errorIregjistruar)
+		{
+			Gabimi +="\nKy Username eshte i regjistruar.";
+			editUsername.setText("");
+			dalja=false;
+		}
+		return dalja;
+		
 	}
 	
+	
+	private void showFinishAlert() {
+		Alert = new AlertDialog.Builder(this);
+		String strTitle = "Regjistrimi perfundoi!";
+		String strMessage = "U regjistruat me sukses!";
+		String strOK = "OK";
+		Alert.setTitle(strTitle);
+		Alert.setMessage(strMessage);
+		Alert.setNeutralButton(strOK,
+				new DialogInterface.OnClickListener() {
+
+					@Override
+					public void onClick(DialogInterface dialog, int which) {
+						Intent i = new Intent(KrijoUser.this,Login.class);
+						startActivity(i);
+					}});
+		
+		Alert.show();
+	}
+	private boolean regex(String expression,String Text)
+	{
+		Pattern pattern = Pattern.compile(expression);
+		Matcher matcher = pattern.matcher(Text);
+		
+		return matcher.matches();
+		
+		
+	}
 	public String gjeneroSalt()
 	{
-		    Random generator = new Random();
-		    StringBuilder randomStringBuilder = new StringBuilder();
-		    
-		    char tempChar;
-		    for (int i = 0; i < 4; i++){
-		        tempChar = (char) (generator.nextInt(96) + 32);
-		        randomStringBuilder.append(tempChar);
-		    }
-		    return randomStringBuilder.toString();
-		
+		Random generator = new Random();
+	    StringBuilder randomStringBuilder = new StringBuilder();
+
+	    char tempChar;
+	    for (int i = 0; i < 4; i++){
+	        tempChar = (char) (generator.nextInt(96) + 32);
+	        randomStringBuilder.append(tempChar);
+	    }
+	    return randomStringBuilder.toString();
 	}
  
 	
@@ -218,14 +297,77 @@ protected String doInBackground(String... params) {
 @Override
 protected void onPostExecute(String teDhenat) {
     // Display the results of the lookup.
-	
+	showFinishAlert();
     }
 }
 
+	private class validouserTask extends
+    AsyncTask<String, Void, String> {
+		
+@Override
+protected String doInBackground(String... params) {
+    
+    /**
+     * /Krijojme objektin e Web Sherbimit
+     */
+    WebserviceCall objThirrja = new WebserviceCall(); 
+
+    /**
+     * /Thirrim Web Sherbimin dhe marrim pergjigjjen.
+     */
+    String argumentet[] = new String[4];
+    
+    argumentet[0]="Username";
+    argumentet[1]=params[0];
+    argumentet[2]="Lloji";
+    argumentet[3]=params[1];
+        
+    String aResponse = objThirrja.thirrMetoden("validoUsername", argumentet); 
+    
+    return aResponse;
+   
+  }
+
+
+
+@Override
+protected void onPostExecute(String teDhenat) {
+    // Display the results of the lookup.
+	String Iregjistruar = teDhenat.substring(0,teDhenat.indexOf(","));
+	String Egziston = teDhenat.substring(teDhenat.indexOf(",")+1);
+	if(Iregjistruar.equals("True"))
+	{
+		errorIregjistruar=true;
+	}
+	if(Egziston.equals("False"))
+	{
+		errorNukEgziston=true;
+	
+	}
+	if(validuesi())
+	{
+	String Username=editUsername.getText().toString();
+	String Hash1 = hash(editPassword.getText().toString());
+	String Salt=gjeneroSalt();
+	Hash1 = Hash1+Salt;
+	String Hash = hash(Hash1);
+	String Email = editEmail.getText().toString();
+	(new shtouserTask()).execute(Username,Hash,Salt,Email);
+	
+	}
+	else
+	{
+		Mesazhi.setText(Gabimi);
+		
+	}
+    }
+}
+	
 	@Override
 	public void onFocusChange(View v, boolean hasFocus) {
 		// TODO Auto-generated method stub
-		if(editUsername.getText().toString()!="")
+		
+		if(!editUsername.isFocused() && editUsername.getText().toString()!="")
 		{
 		objProgress.setVisibility(0);
 		(new lexoTask()).execute("Individ");
